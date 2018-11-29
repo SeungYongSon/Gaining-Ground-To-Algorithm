79334c8f44e8750ed3371665ed1d8ca615f70f6b
Sungyong Son <space010127@gmail.com>
2018-11-30 08:46:07 +0900

    courses 30, lessons 42577

--- C30L42577.java
+++ C30L42577.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 /*
  * https://programmers.co.kr/learn/courses/30/lessons/42577
  */
@@ -8,24 +5,10 @@ import java.util.Comparator;
 class C30L42577 {
 	
 	public boolean solution(String[] phone_book) {		
-		Arrays.sort(phone_book, new Comparator<String>() {
-			@Override
-			public int compare(String s1, String s2) {
-				return s1.length() - s2.length();
-			}
-		});
-		
-		for(int i = 0; i < phone_book.length; i++) {
-			for(int j = 0; j < phone_book.length; j++) {
-				if(i != j) {
-					if(phone_book[j].length() > phone_book[i].length()) {
-						if(phone_book[i].contains(phone_book[j].substring(0, phone_book[i].length()))) 
-							return false;
-					}else {
-						if(phone_book[j].contains(phone_book[i].substring(0, phone_book[j].length()))) 
-							return false;
-					}
-				}
+		for(int i = 0; i < phone_book.length -1; i++) {
+			for(int j = i + 1; j < phone_book.length; j++) {
+				if(phone_book[i].startsWith(phone_book[j])) return false;
+				if(phone_book[j].startsWith(phone_book[i])) return false;
 			}
 		}
 		return true;
